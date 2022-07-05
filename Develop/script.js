4229181eae2ddc2ea2859dd9f28c8c33a26d1c86
Annie Hosch <anniehosch@Annies-Air.lan>
2022-07-04 23:03:45 -0400

    added cards for hours

--- Develop/script.js
+++ Develop/script.js
@@ -1,2 +1,13 @@
 var now = moment().format("MMMM Do YYYY")
 $("#currentDay").text(now);
+
+
+// WHEN I am presented with time blocks for standard business hours
+// WHEN I view the time blocks for that day
+// THEN each time block is color-coded to indicate whether it is in the past, present, or future
+// WHEN I click into a time block
+// THEN I can enter an event
+// WHEN I click the save button for that time block
+// THEN the text for that event is saved in local storage
+// WHEN I refresh the page
+// THEN the saved events persist
